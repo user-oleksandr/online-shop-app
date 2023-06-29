34376c4389840401fe55c5e86c216cf7fdba1383
Oleksandr Anastasiiev <name.oleksandr@gmail.com>
2023-06-29 23:56:21 +0300

    create online shop

---
+++ src/components/products.jsx
@@ -0,0 +1,91 @@
+export const lapTop = [
+    {
+        id: 1,
+        name: 'MacBook Pro',
+        price: 1485,
+        image: 'https://jabko.ua/image/cache//catalog/products/2022/06/070949/Apple_new-macbookpro-wallpaper-s%20(2)full.jpg.webp',
+        description: 'Apple MacBook Pro 13, 512GB, Space Gray with Apple M2 (2022)',
+    },
+    {
+        id: 2,
+        name: 'MacBook Air',
+        price: 1535,
+        image: 'https://jabko.ua/image/cache/catalog/products/2022/06/062308/macbook-air-midnight-gallery1-20-1397x1397.jpg.webp',
+        description: 'MacBook Air 13 Retina, Starlight, 256GB, 8 CPU / 8 GPU, 8GB RAM with Apple M2 (MLY13)',
+    },
+    {
+        id: 3,
+        name: 'IMac',
+        price: 1539,
+        image: 'https://jabko.ua/image/cache//catalog/products/2022/05/181850/apple_new-imac-spring21_hero_042full.jpg.webp',
+        description: 'Apple iMac 24 with Retina 4.5K, 256GB, 8 CPU / 7 GPU',
+    },
+];
+
+
+
+export const tablet = [
+    {
+        id: 1,
+        name: 'iPad Pro',
+        price: 1124,
+        image: 'https://jabko.ua/image/cache//catalog/products/2022/10/211741/ipad-pro-11-select-202210full.jpg.webp',
+        description: 'Apple iPad Pro 12.9 2022, 128GB, Space Gray, Wi-Fi',
+    },
+    {
+        id: 2,
+        name: 'iPad Air',
+        price: 629,
+        image: 'https://jabko.ua/image/cache//catalog/products/2022/05/061649/Apple-iPad-Air-hero-color-lineup-220308full.jpg.webp',
+        description: 'Apple iPad Air, 64GB, Wi-Fi, Space Gray (MM9C3) (2022)',
+    },
+    {
+        id: 3,
+        name: 'iPad',
+        price: 508,
+        image: 'https://jabko.ua/image/cache//catalog/products/2022/10/201843/Apple-iPad-10th-gen-hero-221018full.jpg.webp',
+        description: 'Apple iPad 10.9 64GB, Wi-Fi (Yellow) 2022 (MPQ23)',
+    },
+    {
+        id: 4,
+        name: 'iPad Mini',
+        price: 522,
+        image: 'https://jabko.ua/image/cache//catalog/products/2022/05/041233/2021-09-16%2013.23.03full.jpg.webp',
+        description: 'Планшет Apple iPad mini 6 64GB, Wi-Fi (Space Gray) (MK7M3)',
+    },
+];
+
+
+export const phones = [
+    {
+        id: 1,
+        name: 'iPhone 14',
+        price: 835,
+        image: 'https://jabko.ua/image/cache//catalog/products/2022/09/091734/Apple-iPhone-14-iPhone-14-Plus-hero-220907full.jpg.webp',
+        description: 'Apple iPhone 14 128GB',
+    },
+    {
+        id: 2,
+        name: 'iPhone 13',
+        price: 763,
+        image: 'https://jabko.ua/image/cache/catalog/products/2022/03/180929/iphone-13-2022-gallery-2_GEO_US%20(1)-1397x1397.jpg.webp',
+        description: 'Apple iPhone 13 128GB',
+    },
+    {
+        id: 3,
+        name: 'iPhone 12',
+        price: 645,
+        image: 'https://jabko.ua/image/cache//catalog/products/2022/06/031208/apple_iphone12mini-iphone12max-h%20(1)full.jpeg.webp',
+        description: 'Apple iPhone 12 64GB',
+    },
+    {
+        id: 4,
+        name: 'iPhone SE',
+        price: 435,
+        image: 'https://jabko.ua/image/cache//catalog/products/2022/05/051733/Apple_new-iphone-se-black-white-product-red-colors_04152020full.jpg.webp',
+        description: 'Apple iPhone SE 64GB',
+    },
+];
+
+
+
